a6ff54c5c70c7cdc317a97f67b9bfb9be8c907a9
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-03-14 00:06:00 -0300

    App.tsx: Theme and Fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,37 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Home } from "screens/Home";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import { ThemeProvider } from "styled-components";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+
+import { Home } from "./src/screens/Home";
+
+import theme from "./src/styles/theme";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
