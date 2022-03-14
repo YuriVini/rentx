cfe8bef6448f28ee72e802275ae85eef0cef64a4
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-03-14 00:02:46 -0300

    SVG configs @types

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,14 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig(__dirname);
+
   return {
     transformer: {
-      babelTransformersPath: require.resolve("react-native-svg-transformer"),
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== "svg"),
