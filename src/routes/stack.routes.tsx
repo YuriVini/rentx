824122f007755970351b012150f31fdf7e9dbf7b
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-04-21 01:44:47 -0300

    Added MyCars Screen to routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
+import { MyCars } from "screens/MyCars";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -14,6 +15,7 @@ export function StackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} />
+      <Screen name="MyCars" component={MyCars} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
